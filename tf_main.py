f3cc5b4e36230bef5d5d0ce7c37becafa84852e4
Red-Portal <msca8h@naver.com>
2018-06-01 14:44:18 +0900

    smaller batch..

--- tf_main.py
+++ tf_main.py
@@ -67,7 +67,7 @@ def main():
     report_step = 10
     validation_step = 100
     total_iteration = 1000
-    batch_size = 16
+    batch_size = 8
 
     data, label = load_data("./dataset/imgs", "./dataset/masks")
     print("data: ", len(data), " shape: ", data[0].shape)
