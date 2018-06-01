a6656fd0f8bf7974679c4f579b38c525792e6272
Red-Portal <msca8h@naver.com>
2018-06-01 14:34:59 +0900

    fixed

--- tf_main.py
+++ tf_main.py
@@ -14,8 +14,8 @@ def soft_jaccard(output, target, axis=(1, 2, 3), smooth=1e-5):
     return dice
 
 def hard_jaccard(output, target, axis=(1, 2, 3), smooth=1e-5):
-    pre = tf.cast(output > threshold, dtype=tf.float32)
-    truth = tf.cast(target > threshold, dtype=tf.float32)
+    pre = tf.cast(output > 0.5, dtype=tf.float32)
+    truth = tf.cast(target > 0.5, dtype=tf.float32)
     inse = tf.reduce_sum(tf.multiply(pre, truth), axis=axis)  # AND
     union = tf.reduce_sum(tf.cast(tf.add(pre, truth) >= 1, dtype=tf.float32), axis=axis)  # OR
     batch_iou = (inse + smooth) / (union + smooth)
