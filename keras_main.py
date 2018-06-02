9a28426dbb0381ab73a4b587bb77617b8352e770
Red-Portal <msca8h@naver.com>
2018-06-02 19:58:03 +0900

    using jaccard loss

--- keras_main.py
+++ keras_main.py
@@ -8,7 +8,7 @@ import os
 from custom_losses import *
 from keras_model import CapsNetR3
 
-def jaccard_distance(y_true, y_pred, smooth=1e-5):
+def hard_jaccard(y_true, y_pred, smooth=1e-5):
     y_true = keras.backend.batch_flatten(y_true)
 
     y_pred = keras.backend.batch_flatten(y_pred)
@@ -23,6 +23,30 @@ def jaccard_distance(y_true, y_pred, smooth=1e-5):
 
     jac = (inter + smooth) / (union + smooth)
     return keras.backend.mean(jac)
+
+def soft_jaccard(y_true, y_pred, smooth=100):
+    """Jaccard distance for semantic segmentation, also known as the intersection-over-union loss.
+    This loss is useful when you have unbalanced numbers of pixels within an image
+    because it gives all classes equal weight. However, it is not the defacto
+    standard for image segmentation.
+    For example, assume you are trying to predict if each pixel is cat, dog, or background.
+    You have 80% background pixels, 10% dog, and 10% cat. If the model predicts 100% background
+    should it be be 80% right (as with categorical cross entropy) or 30% (with this loss)?
+    The loss has been modified to have a smooth gradient as it converges on zero.
+    This has been shifted so it converges on 0 and is smoothed to avoid exploding
+    or disappearing gradient.
+    Jaccard = (|X & Y|)/ (|X|+ |Y| - |X & Y|)
+            = sum(|A*B|)/(sum(|A|)+sum(|B|)-sum(|A*B|))
+    # References
+    Csurka, Gabriela & Larlus, Diane & Perronnin, Florent. (2013).
+    What is a good evaluation measure for semantic segmentation?.
+    IEEE Trans. Pattern Anal. Mach. Intell.. 26. . 10.5244/C.27.32.
+    https://en.wikipedia.org/wiki/Jaccard_index
+    """
+    intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
+    sum_ = K.sum(K.abs(y_true) + K.abs(y_pred), axis=-1)
+    jac = (intersection + smooth) / (sum_ - intersection + smooth)
+    return (1 - jac) * smooth
 
 def onehot(output):
     return tf.cast(output > 0.5, dtype=tf.float32)
@@ -60,7 +84,7 @@ def main():
     model = keras.models.Model(inputs=input_layer, outputs=model)
     opt = keras.optimizers.Nadam(lr=lr)
     loss = "binary_crossentropy"#margin_loss(margin=0.4, downweight=0.5, pos_weight=1.0)
-    model.compile(optimizer=opt, loss=loss, metrics=[jaccard_distance])
+    model.compile(optimizer=opt, loss=soft_jaccard, metrics=[hard_jaccard])
     model.fit(data, label,
               validation_split=validation_split,
               #validation_step=validation_step,
