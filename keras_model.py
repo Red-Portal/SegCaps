81302e7f3f608efe7ede8bde8449985f9e655151
Red-Portal <msca8h@naver.com>
2018-06-02 20:05:50 +0900

    sigmoid

--- keras_model.py
+++ keras_model.py
@@ -85,5 +85,5 @@ def CapsNetR3(input_layer, n_class=2):
     #out_seg = Length(num_classes=n_class, seg=True, name='out_seg')(seg_caps)
     #out_seg = Length(num_classes=n_class, seg=True, name='out_seg')(seg_caps)
 
-    conv1 = layers.Conv2D(filters=1, kernel_size=5, strides=1, padding='same', activation='relu', name='conv_last')(conv1)
+    conv1 = layers.Conv2D(filters=1, kernel_size=5, strides=1, padding='same', activation='sigmoid', name='conv_last')(conv1)
     return conv1
