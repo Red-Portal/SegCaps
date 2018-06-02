2e3b92650b7fc031649a90a31933f9043372b42c
Red-Portal <msca8h@naver.com>
2018-06-02 16:32:09 +0900

    done

--- keras_main.py
+++ keras_main.py
@@ -31,9 +31,12 @@ def jaccard_distance(y_true, y_pred, smooth=1e-5):
     y_pred = keras.backend.batch_flatten(y_pred)
     y_true = keras.backend.cast(keras.backend.greater(y_true, 1.0), "float32")
     y_pred = keras.backend.cast(keras.backend.greater(y_pred, 1.0), "float32")
-    inter = y_true * y_pred
-    union = keras.backend.sum(y_true + y_pred, axis=-1)
-    jac = (inter + smooth) / (union - intersection + smooth)
+    inter = keras.backend.sum(y_true * y_pred)
+
+    union = y_true + y_pred
+    union = keras.backend.cast(keras.backend.greater(union, 1.0), "float32")
+    union = keras.backend.sum(union, axis=-1)
+    jac = (inter + smooth) / (union + smooth)
     return jac
 
 def onehot(output):
