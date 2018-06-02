055a0b5cce44bb89a9b97df8834cbf94ffc9bf83
Red-Portal <msca8h@naver.com>
2018-06-02 19:54:08 +0900

    proper jaccard

--- keras_main.py
+++ keras_main.py
@@ -9,33 +9,17 @@ from custom_losses import *
 from keras_model import CapsNetR3
 
 def jaccard_distance(y_true, y_pred, smooth=1e-5):
-    """Jaccard distance for semantic segmentation, also known as the intersection-over-union loss.
-    This loss is useful when you have unbalanced numbers of pixels within an image
-    because it gives all classes equal weight. However, it is not the defacto
-    standard for image segmentation.
-    For example, assume you are trying to predict if each pixel is cat, dog, or background.
-    You have 80% background pixels, 10% dog, and 10% cat. If the model predicts 100% background
-    should it be be 80% right (as with categorical cross entropy) or 30% (with this loss)?
-    The loss has been modified to have a smooth gradient as it converges on zero.
-    This has been shifted so it converges on 0 and is smoothed to avoid exploding
-    or disappearing gradient.
-    Jaccard = (|X & Y|)/ (|X|+ |Y| - |X & Y|)
-            = sum(|A*B|)/(sum(|A|)+sum(|B|)-sum(|A*B|))
-    # References
-    Csurka, Gabriela & Larlus, Diane & Perronnin, Florent. (2013).
-    What is a good evaluation measure for semantic segmentation?.
-    IEEE Trans. Pattern Anal. Mach. Intell.. 26. . 10.5244/C.27.32.
-    https://en.wikipedia.org/wiki/Jaccard_index
-    """
     y_true = keras.backend.batch_flatten(y_true)
 
     y_pred = keras.backend.batch_flatten(y_pred)
     y_pred = keras.backend.cast(keras.backend.greater(y_pred, 0.5), "float32")
 
     inter = y_true * y_pred
+    inter = keras.backend.sum(inter, axis=-1)
 
     union = y_true + y_pred
     union = keras.backend.cast(keras.backend.greater(union, 0.5), "float32")
+    union = keras.backend.sum(union, axis=-1)
 
     jac = (inter + smooth) / (union + smooth)
     return keras.backend.mean(jac)
